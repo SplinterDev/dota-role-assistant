32bf432c6ed315c44a720e2b13366ba25f42aa60
Fabrício Montenegro <fabriciojcmontenegro@gmail.com>
2021-06-12 08:09:39 -0300

    add heroes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React, { useEffect } from 'react'
-import * as heroes from './data/allHeroes.json'
+import React from 'react'
+import heroes from './data/allHeroes.json'
 import './App.css'
 
 function App(): React.ReactElement {
-  return <div className="App">hello</div>
+  return (
+    <div className="App">
+      <ul>
+        {heroes.map((item) => {
+          const imageUrl =
+            'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/'
+          const imageName = item.name.slice(14) + '.png'
+          return (
+            <li key={item.id}>
+              {item.name_loc}
+              <img src={`${imageUrl}${imageName}`} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default App

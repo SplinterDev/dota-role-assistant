d3b8a9109d0a3cd6a2b8f234095e77b0598e55ae
SplinterDev <fabriciojcmontenegro@gmail.com>
2021-06-13 01:19:00 -0300

    Add hero details

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,6 +7,19 @@ type Props = {
 }
 
 const Hero = ({ hero }: Props): React.ReactElement => {
+  const renderComplexity = (complexity: number): JSX.Element[] => {
+    const indicators = []
+    for (let i = 0; i < 3; i++) {
+      indicators.push(
+        <div
+          key={i}
+          className={`complexity-indicator ${complexity > i ? 'filled' : ''}`}
+        ></div>,
+      )
+    }
+    return indicators
+  }
+
   return (
     <li className="Hero">
       <img className="avatar" src={hero.image} />
@@ -16,6 +29,26 @@ const Hero = ({ hero }: Props): React.ReactElement => {
           src={`https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_${hero.attribute}.png`}
         />
         <h2>{hero.name}</h2>
+      </div>
+      <div className="hero-details">
+        <div className="hero-complexity">
+          Complexity: {renderComplexity(hero.complexity)}
+        </div>
+        <div className="roles-list">
+          {hero.roles.Carry > 0 && <div className="roles-item">Carry</div>}
+          {hero.roles.Support > 0 && <div className="roles-item">Support</div>}
+          {hero.roles.Nuker > 0 && <div className="roles-item">Nuker</div>}
+          {hero.roles.Disabler > 0 && (
+            <div className="roles-item">Disabler</div>
+          )}
+          {hero.roles.Jungler > 0 && <div className="roles-item">Jungler</div>}
+          {hero.roles.Durable > 0 && <div className="roles-item">Durable</div>}
+          {hero.roles.Escape > 0 && <div className="roles-item">Escape</div>}
+          {hero.roles.Pusher > 0 && <div className="roles-item">Pusher</div>}
+          {hero.roles.Initiator > 0 && (
+            <div className="roles-item">Initiator</div>
+          )}
+        </div>
       </div>
     </li>
   )
